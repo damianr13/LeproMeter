7d61787c306c713dc6cb5a73dbde545620cc501f
Horia Leonte <horialeonte@gmail.com>
2020-04-15 23:54:25 +0200

    Login / Register working

--- app/src/main/java/com/bigdict/leprometer/loginActivity.kt
+++ app/src/main/java/com/bigdict/leprometer/loginActivity.kt
@@ -3,9 +3,13 @@ package com.bigdict.leprometer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class loginActivity : AppCompatActivity() {
 
@@ -15,13 +19,27 @@ class loginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         auth = FirebaseAuth.getInstance()
 
-        gotoRegisterButton.setOnClickListener(){
+        gotoRegisterButton.setOnClickListener() {
             startActivity((Intent(this, registerActivity::class.java)))
             finish()
         }
 
+        signButton.setOnClickListener() {
+            doLogin()
+        }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?){
+        if(currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(baseContext, "Login Failed",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 
     public override fun onStart() {
@@ -31,9 +49,33 @@ class loginActivity : AppCompatActivity() {
     }
 
 
-    fun updateUI(currentUser : FirebaseUser?){
+    private fun doLogin(){
+
+        if(nameLoginEditText.text.toString().isEmpty()){
+            nameLoginEditText.error = "Please enter a valid email address"
+            nameLoginEditText.requestFocus()
+            return
+        }
+
+        if(passwordLoginEditText.text.toString().isEmpty()){
+            passwordLoginEditText.error = "Please enter a password"
+            passwordLoginEditText.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(nameLoginEditText.text.toString(), passwordLoginEditText.text.toString())
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful){
+                    val user = auth.currentUser
+                    updateUI(user)
+                }   else{
+
+                    Toast.makeText(baseContext, "Login Failed",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
     }
-
-
-
 }
+
+
